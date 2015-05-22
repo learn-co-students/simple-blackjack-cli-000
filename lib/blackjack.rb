8f30b2dbe004bbf316fd1277c2abc088b1e3dd70
jonhunter13 <jonhunter13@gmail.com>
2015-05-22 15:33:44 -0400

    Blackjacking til you lose

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,57 @@
 def deal_card
   # code #deal_card here
+  1 + rand(11)
 end
 
-def display_card_total
+def display_card_total(cards)
   # code #display_card_total here
+  puts "Your cards add up to #{cards}"
 end
 
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def initial_round
   # code #initial_round here
+  cards = deal_card + deal_card
+  display_card_total(cards)
+  cards
 end
 
-def end_game
+def end_game(cards)
   # code #end_game here
+  puts "Sorry, you hit #{cards}. Thanks for playing!"
 end
 
 def get_user_input
   # code #get_user_input here
+  $stdin.gets.chomp
 end
 
-def hit?
+def hit?(cards)
   # code hit? here
+  prompt_user
+  input = get_user_input
+  if(input == 'h')
+    cards += deal_card
+  elsif(input == 's')
+    cards
+  else
+    invalid_command(cards)
+  end
 end
 
-def invalid_command
+def invalid_command(cards)
   # code invalid_command here
+  puts "Invalid command, please try again"
+  hit?(cards)
 end
 
 #####################################################
@@ -40,5 +60,13 @@ end
 
 def runner
   # code runner here
+  welcome
+  cards = initial_round
+  until cards >= 21 do
+    cards = hit?(cards)
+    display_card_total(cards)
+  end
+  end_game(cards)
 end
+
     
