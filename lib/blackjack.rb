49884da36fdbafad9fb7fd037054769c3abbdc40
jvenator <j@venator.vc>
2015-11-06 01:00:56 -0500

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,66 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  card = (1..11).to_a.sample
+  return card
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  card_sum = 0
+
+  2.times do
+    card_sum += deal_card
+  end
+
+  display_card_total(card_sum)
+  return card_sum
 end
 
-def hit?
+def hit?(card_total)
   # code hit? here
+  prompt_user
+  response = get_user_input
+
+  if response == "h"
+    result = card_total + deal_card
+  elsif response == "s"
+    result = card_total
+  else
+    invalid_command
+    prompt_user
+  end
+
+  return result
 end
 
 def invalid_command
   # code invalid_command here
+  puts  "Please enter a valid command"
 end
 
 #####################################################
@@ -40,5 +69,13 @@ end
 
 def runner
   # code runner here
+  welcome
+  hand = initial_round
+  until hand > 21 do
+    hand = hit?(hand)
+    display_card_total(hand)
+  end
+
+  end_game(hand)
 end
     
